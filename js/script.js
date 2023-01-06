8fd9e46231d1dc618104a8c340b36d232e7d6cbd
Vova <example@gmail.com>
2023-01-06 11:35:38 +0200

    test commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here ?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "18");
+//console.log(answer + 5);//например так показывает что строка
+
+/*const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null));*/
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user ="Ivan";
+
+alert(`Привет, ${user}`);
